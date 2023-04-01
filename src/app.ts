fd66a636a1de331f843543f1e1c2ea70491f91f8
lucastdelara <lucastlara2506@gmail.com>
2023-04-01 16:06:34 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+
+// import { errorIdentify } from "./errors/appError";
+// import userRoutes from "./routers/users.router";
+// import { loginRouter } from "./routers/login.router";
+// import postsRoutes from './routers/posts.router';
+// import commentRoutes from './routers/comment.router';
+// import productRouter from './routers/products.router';
+
+export const app = express();
+
+app.use(express.json());
+
+// app.use('/users', userRoutes);
+
+// app.use('/posts', postsRoutes);
+
+// app.use('/login', loginRouter);
+
+// app.use('/message', commentRoutes);
+
+// app.use('/products', productRouter);
+
+// app.use(errorIdentify);
