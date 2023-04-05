2c1ca93d399d62662b455f7bbb38704899132a88
lucastdelara <lucastlara2506@gmail.com>
2023-04-05 16:02:25 -0300

    delivery

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import cors from "cors";
 
 import { errorIdentify } from "./errors/appError";
 import userRoutes from "./routes/users.router";
@@ -10,6 +11,8 @@ import contactRoutes from "./routes/contacts.router";
 export const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/login', loginRoutes);
 
